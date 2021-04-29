490430b3bcedf6f1b11a35a010e1fa52ede33a36
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-04-29 11:36:33 +0300

    fedora-installer-iso.sh

---
+++ fedora-installer-iso.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo "You've started installation Fedora on Termux"
+apt update -y
+apt upgrade -y
+pkg update -y
+pkg upgrade -y
+pkg install curl -y
+pkg install proot -y
+pkg install tar -y
+curl https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Fedora/fedora.sh | bash
+rm -rf fedora-installer-iso.sh
+chmod +x *
+./start-fedora.sh
+apt update -y
+apt upgrade -y
+apt install sudo -y
+sudo -i
+apt install python -y
+apt install python2 -y
+apt install python3 -y
+apt install git -y
+apt install wget -y
+apt install curl -y
+apt install pip -y
+apt install python-pip -y
+apt install python3-pip -y
+echo "Fedora has been installed"
