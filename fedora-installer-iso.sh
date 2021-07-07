c998c4f728e17f16cd1494760ed026151fab3b1f
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-07 12:00:12 +0400

    fedora-installer-iso.sh

--- fedora-installer-iso.sh
+++ fedora-installer-iso.sh
@@ -9,20 +9,5 @@ pkg install proot -y
 pkg install tar -y
 cd ~/
 curl https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Fedora/fedora.sh | bash
-chmod +x *
-./start-fedora.sh
-apt update -y
-apt upgrade -y
-apt install sudo -y
-sudo -i
-apt install python -y
-apt install python2 -y
-apt install python3 -y
-apt install git -y
-apt install wget -y
-apt install curl -y
-apt install pip -y
-apt install python-pip -y
-apt install python3-pip -y
 echo "Fedora has been installed"
-rm -rf fedora-installer-iso.sh
+rm -rf Fedora
